277ea3c8e8cdf72f8756573bcab171ce6a18479d
Boris Poloshkov <bpoloshkov@ozon.ru>
2024-12-24 12:09:56 +0300

    leave only hierarchy

--- students/be-poloshkov/lab1/run.py
+++ students/be-poloshkov/lab1/run.py
@@ -183,93 +183,93 @@ def main():
     inter_dist = mean_intercluster_distance(countries_clustered_data, my_aggl_labels)
     print(f'Time on MyAgglomerative is {my_aggl_end_time - my_aggl_start_time}, intra distance: {intra_dist}, inter distance: {inter_dist}')
 
-    # DBSCAN
-    my_dbscan = MyDBSCAN(metric='euclidean', min_samples=5, eps=10)
-    my_dbscan_start_time = time.time()
-    my_dbscan_labels = my_dbscan.fit_predict(countries_clustered_data)
-    my_dbscan_end_time = time.time()
-    intra_dist = mean_intracluster_distance(countries_clustered_data, my_dbscan_labels)
-    inter_dist = mean_intercluster_distance(countries_clustered_data, my_dbscan_labels)
-    print(f'Time on MyDBSCAN Countries is {my_dbscan_end_time - my_dbscan_start_time}, , intra distance: {intra_dist}, inter distance: {inter_dist}')
+    # # DBSCAN
+    # my_dbscan = MyDBSCAN(metric='euclidean', min_samples=5, eps=10)
+    # my_dbscan_start_time = time.time()
+    # my_dbscan_labels = my_dbscan.fit_predict(countries_clustered_data)
+    # my_dbscan_end_time = time.time()
+    # intra_dist = mean_intracluster_distance(countries_clustered_data, my_dbscan_labels)
+    # inter_dist = mean_intercluster_distance(countries_clustered_data, my_dbscan_labels)
+    # print(f'Time on MyDBSCAN Countries is {my_dbscan_end_time - my_dbscan_start_time}, , intra distance: {intra_dist}, inter distance: {inter_dist}')
+    #
+    # sk_dbscan = DBSCAN(eps=10, min_samples=5, metric='euclidean')
+    # sk_dbscan_start_time = time.time()
+    # sk_dbscan_labels = sk_dbscan.fit_predict(countries_clustered_data)
+    # sk_dbscan_end_time = time.time()
+    # intra_dist = mean_intracluster_distance(countries_clustered_data, sk_dbscan_labels)
+    # inter_dist = mean_intercluster_distance(countries_clustered_data, sk_dbscan_labels)
+    # print(f'Time on SK DBSCAN Countries is {sk_dbscan_end_time - sk_dbscan_start_time}, , intra distance: {intra_dist}, inter distance: {inter_dist}')
 
-    sk_dbscan = DBSCAN(eps=10, min_samples=5, metric='euclidean')
-    sk_dbscan_start_time = time.time()
-    sk_dbscan_labels = sk_dbscan.fit_predict(countries_clustered_data)
-    sk_dbscan_end_time = time.time()
-    intra_dist = mean_intracluster_distance(countries_clustered_data, sk_dbscan_labels)
-    inter_dist = mean_intercluster_distance(countries_clustered_data, sk_dbscan_labels)
-    print(f'Time on SK DBSCAN Countries is {sk_dbscan_end_time - sk_dbscan_start_time}, , intra distance: {intra_dist}, inter distance: {inter_dist}')
+    # # DBSCAN Wine
+    # my_dbscan_wine = MyDBSCAN(metric='euclidean', min_samples=5, eps=5)
+    # my_dbscan_start_time_wine = time.time()
+    # my_dbscan_labels_wine = my_dbscan_wine.fit_predict(wine_data_tsne)
+    # my_dbscan_end_time_wine = time.time()
+    # intra_dist = mean_intracluster_distance(wine_data_tsne, my_dbscan_labels_wine)
+    # inter_dist = mean_intercluster_distance(wine_data_tsne, my_dbscan_labels_wine)
+    # print(f'Time on MyDBSCAN Wine is {my_dbscan_end_time_wine - my_dbscan_start_time_wine}, , intra distance: {intra_dist}, inter distance: {inter_dist}')
+    #
+    # sk_dbscan_wine = DBSCAN(eps=5, min_samples=5, metric='euclidean')
+    # sk_dbscan_start_time_wine = time.time()
+    # sk_dbscan_labels_wine = sk_dbscan_wine.fit_predict(wine_data_tsne)
+    # sk_dbscan_end_time_wine = time.time()
+    # intra_dist = mean_intracluster_distance(wine_data_tsne, sk_dbscan_labels_wine)
+    # inter_dist = mean_intercluster_distance(wine_data_tsne, sk_dbscan_labels_wine)
+    # print(f'Time on SK DBSCAN Wine is {sk_dbscan_end_time_wine - sk_dbscan_start_time_wine}, , intra distance: {intra_dist}, inter distance: {inter_dist}')
 
-    # DBSCAN Wine
-    my_dbscan_wine = MyDBSCAN(metric='euclidean', min_samples=5, eps=5)
-    my_dbscan_start_time_wine = time.time()
-    my_dbscan_labels_wine = my_dbscan_wine.fit_predict(wine_data_tsne)
-    my_dbscan_end_time_wine = time.time()
-    intra_dist = mean_intracluster_distance(wine_data_tsne, my_dbscan_labels_wine)
-    inter_dist = mean_intercluster_distance(wine_data_tsne, my_dbscan_labels_wine)
-    print(f'Time on MyDBSCAN Wine is {my_dbscan_end_time_wine - my_dbscan_start_time_wine}, , intra distance: {intra_dist}, inter distance: {inter_dist}')
-
-    sk_dbscan_wine = DBSCAN(eps=5, min_samples=5, metric='euclidean')
-    sk_dbscan_start_time_wine = time.time()
-    sk_dbscan_labels_wine = sk_dbscan_wine.fit_predict(wine_data_tsne)
-    sk_dbscan_end_time_wine = time.time()
-    intra_dist = mean_intracluster_distance(wine_data_tsne, sk_dbscan_labels_wine)
-    inter_dist = mean_intercluster_distance(wine_data_tsne, sk_dbscan_labels_wine)
-    print(f'Time on SK DBSCAN Wine is {sk_dbscan_end_time_wine - sk_dbscan_start_time_wine}, , intra distance: {intra_dist}, inter distance: {inter_dist}')
-
-    # EM
-    my_em = MyEM(n_clusters=3, max_iter=1000)
-    my_em_start_time = time.time()
-    my_em_labels = my_em.fit_predict(countries_clustered_data)
-    my_em_end_time = time.time()
-    intra_dist = mean_intracluster_distance(countries_clustered_data, my_em_labels)
-    inter_dist = mean_intercluster_distance(countries_clustered_data, my_em_labels)
-    print(f'Time on MyEM Countries is {my_em_end_time - my_em_start_time}, , intra distance: {intra_dist}, inter distance: {inter_dist}')
-
-    sk_em = GaussianMixture(n_components=3, covariance_type='full')
-    sk_em_start_time = time.time()
-    sk_em_labels = sk_em.fit_predict(countries_clustered_data)
-    sk_em_end_time = time.time()
-    intra_dist = mean_intracluster_distance(countries_clustered_data, sk_em_labels)
-    inter_dist = mean_intercluster_distance(countries_clustered_data, sk_em_labels)
-    print(f'Time on SK EM Countries is {sk_em_end_time - sk_em_start_time}, intra distance: {intra_dist}, inter distance: {inter_dist}')
-
-    # EM Wine
-    my_em_wine = MyEM(n_clusters=3, max_iter=100)
-    my_em_start_time_wine = time.time()
-    my_em_labels_wine = my_em_wine.fit_predict(wine_data_tsne)
-    my_em_end_time_wine = time.time()
-    intra_dist = mean_intracluster_distance(wine_data_tsne, my_em_labels_wine)
-    inter_dist = mean_intercluster_distance(wine_data_tsne, my_em_labels_wine)
-    print(f'Time on MyEM Wine is {my_em_end_time_wine - my_em_start_time_wine}, intra distance: {intra_dist}, inter distance: {inter_dist}')
-
-    sk_em_wine = GaussianMixture(n_components=3, covariance_type='full')
-    sk_em_start_time_wine = time.time()
-    sk_em_labels_wine = sk_em_wine.fit_predict(wine_data_tsne)
-    sk_em_end_time_wine = time.time()
-    intra_dist = mean_intracluster_distance(wine_data_tsne, sk_em_labels_wine)
-    inter_dist = mean_intercluster_distance(wine_data_tsne, sk_em_labels_wine)
-    print(f'Time on SK EM Wine is {sk_em_end_time_wine - sk_em_start_time_wine}, intra distance: {intra_dist}, inter distance: {inter_dist}')
+    # # EM
+    # my_em = MyEM(n_clusters=3, max_iter=1000)
+    # my_em_start_time = time.time()
+    # my_em_labels = my_em.fit_predict(countries_clustered_data)
+    # my_em_end_time = time.time()
+    # intra_dist = mean_intracluster_distance(countries_clustered_data, my_em_labels)
+    # inter_dist = mean_intercluster_distance(countries_clustered_data, my_em_labels)
+    # print(f'Time on MyEM Countries is {my_em_end_time - my_em_start_time}, , intra distance: {intra_dist}, inter distance: {inter_dist}')
+    #
+    # sk_em = GaussianMixture(n_components=3, covariance_type='full')
+    # sk_em_start_time = time.time()
+    # sk_em_labels = sk_em.fit_predict(countries_clustered_data)
+    # sk_em_end_time = time.time()
+    # intra_dist = mean_intracluster_distance(countries_clustered_data, sk_em_labels)
+    # inter_dist = mean_intercluster_distance(countries_clustered_data, sk_em_labels)
+    # print(f'Time on SK EM Countries is {sk_em_end_time - sk_em_start_time}, intra distance: {intra_dist}, inter distance: {inter_dist}')
+    #
+    # # EM Wine
+    # my_em_wine = MyEM(n_clusters=3, max_iter=100)
+    # my_em_start_time_wine = time.time()
+    # my_em_labels_wine = my_em_wine.fit_predict(wine_data_tsne)
+    # my_em_end_time_wine = time.time()
+    # intra_dist = mean_intracluster_distance(wine_data_tsne, my_em_labels_wine)
+    # inter_dist = mean_intercluster_distance(wine_data_tsne, my_em_labels_wine)
+    # print(f'Time on MyEM Wine is {my_em_end_time_wine - my_em_start_time_wine}, intra distance: {intra_dist}, inter distance: {inter_dist}')
+    #
+    # sk_em_wine = GaussianMixture(n_components=3, covariance_type='full')
+    # sk_em_start_time_wine = time.time()
+    # sk_em_labels_wine = sk_em_wine.fit_predict(wine_data_tsne)
+    # sk_em_end_time_wine = time.time()
+    # intra_dist = mean_intracluster_distance(wine_data_tsne, sk_em_labels_wine)
+    # inter_dist = mean_intercluster_distance(wine_data_tsne, sk_em_labels_wine)
+    # print(f'Time on SK EM Wine is {sk_em_end_time_wine - sk_em_start_time_wine}, intra distance: {intra_dist}, inter distance: {inter_dist}')
 
     wine_data_tsne.copy()
     # Plotting
     plot_countries('Countries MyAgglomerative ', my_aggl_labels, countries_clustered_data)
     plot_countries('Countries SKAgglomerative ', countries_sk_labels, countries_clustered_data)
 
-    plot_countries('Countries MyDBSCAN', my_dbscan_labels, countries_clustered_data)
-    plot_countries('Countries SKDBSCAN', sk_dbscan_labels, countries_clustered_data)
-
-    plot_countries('Countries MyEM ', my_em_labels, countries_clustered_data)
-    plot_countries('Countries SKEM ', sk_em_labels, countries_clustered_data)
+    # plot_countries('Countries MyDBSCAN', my_dbscan_labels, countries_clustered_data)
+    # plot_countries('Countries SKDBSCAN', sk_dbscan_labels, countries_clustered_data)
+    #
+    # plot_countries('Countries MyEM ', my_em_labels, countries_clustered_data)
+    # plot_countries('Countries SKEM ', sk_em_labels, countries_clustered_data)
 
     plot_wine('Wine MyAgglomerative ', wine_my_hierarchy_labels, wine_data_tsne)
     plot_wine('Wine SkAgglomerative', wine_sk_hierarchy_labels, wine_data_tsne)
 
-    plot_wine('Wine MyDBSCAN', my_dbscan_labels_wine, wine_data_tsne)
-    plot_wine('Wine SKDBSCAN', sk_dbscan_labels_wine, wine_data_tsne)
-
-    plot_wine('Wine MyEM ', my_em_labels_wine, wine_data_tsne)
-    plot_wine('Wine SKEM ', sk_em_labels_wine, wine_data_tsne)
+    # plot_wine('Wine MyDBSCAN', my_dbscan_labels_wine, wine_data_tsne)
+    # plot_wine('Wine SKDBSCAN', sk_dbscan_labels_wine, wine_data_tsne)
+    #
+    # plot_wine('Wine MyEM ', my_em_labels_wine, wine_data_tsne)
+    # plot_wine('Wine SKEM ', sk_em_labels_wine, wine_data_tsne)
 
 
 if __name__ == '__main__':
